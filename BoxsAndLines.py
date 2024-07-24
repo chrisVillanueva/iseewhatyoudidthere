2a8709129416969c99ba148d2a2b34d22ab8ee4a
Seth Anderson <s2and328@gmail.com>
2024-07-24 18:17:33 -0500

    added a menu scelaton

--- BoxsAndLines.py
+++ BoxsAndLines.py
@@ -3,17 +3,18 @@ import random
 
 pygame.init()
 
-pygame.display.set_caption("")
+pygame.display.set_caption("Boxes and Lines")
 screen = pygame.display.set_mode((1020,780))
-screen.fill((0,0,0))
+screen.fill((50, 80, 50))
 font = pygame.font.Font(None, 36)
 TickSpeed = 50
 clock = pygame.time.Clock()
 clock.tick(TickSpeed)
 run = True
+GameStage = "menu"
 
-pygame.draw.rect(screen, (100, 100, 100), [10, 10, 100, 100])
 
+pygame.display.update()
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -21,5 +22,14 @@ while run:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 run = False
+    
+    if GameStage == "menu":
+        
+        pygame.draw.rect(screen, (70, 70, 70), [340, 250, 340, 100], 0, 10)
+        pygame.draw.rect(screen, (70, 70, 70), [340, 400, 340, 100], 0, 10)
+        pygame.draw.rect(screen, (70, 70, 70), [340, 550, 340, 100], 0, 10)
+
+
+    pygame.display.update()
                 
 pygame.quit()
