21c1ad4f7a86abc43910cb2aef23b343e8f4a98e
Seth Anderson <s2and328@gmail.com>
2024-07-30 17:44:52 -0500

    switching to main

--- BoxsAndLines.py
+++ BoxsAndLines.py
@@ -6,15 +6,13 @@ pygame.init()
 pygame.display.set_caption("Boxes and Lines")
 screen = pygame.display.set_mode((1020,780))
 screen.fill((50, 80, 50))
-font = pygame.font.Font(None, 36)
+font = pygame.font.Font(None, 100)
 TickSpeed = 50
 clock = pygame.time.Clock()
 clock.tick(TickSpeed)
 run = True
-GameStage = "menu"
+GameState = "menu"
 
-
-pygame.display.update()
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,13 +21,16 @@ while run:
             if event.key == pygame.K_ESCAPE:
                 run = False
     
-    if GameStage == "menu":
-        
-        pygame.draw.rect(screen, (70, 70, 70), [340, 250, 340, 100], 0, 10)
-        pygame.draw.rect(screen, (70, 70, 70), [340, 400, 340, 100], 0, 10)
-        pygame.draw.rect(screen, (70, 70, 70), [340, 550, 340, 100], 0, 10)
+    if GameState == "menu":
+        pygame.draw.rect(screen, (130, 130, 130), [340, 250, 340, 100], 0, 10)
+        pygame.draw.rect(screen, (130, 130, 130), [340, 400, 340, 100], 0, 10)
+        pygame.draw.rect(screen, (130, 130, 130), [340, 550, 340, 100], 0, 10)
+        screen.blit(font.render("Play", True, (0, 0, 0)), [340, 550], )
 
-
+    elif GameState == "Testing":
+        pass
+    
+    
     pygame.display.update()
                 
 pygame.quit()
